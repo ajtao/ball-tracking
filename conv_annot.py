2b7f2307ef73b960342b08a12511dbb31e9f0d24
stevearonson <steve_aronson@yahoo.com>
2022-02-14 10:53:55 -0500

    multi file annotation conversion
    multi file annotation conversion

--- conv_annot.py
+++ conv_annot.py
@@ -12,40 +12,44 @@ import cv2
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("annot", help="MOT format text annotation file")
+    parser.add_argument("raw_data_dir", help="Directory with video and text annotations")
     args = parser.parse_args()
 
-    pre, ext = os.path.splitext(args.annot)
-    video_path = pre + '.mp4'
-    annot_path = pre + '.csv'
+    for filename in os.listdir(args.raw_data_dir):
 
-    # open the video file to determine frame size
-    cap = cv2.VideoCapture(video_path)
-    frame_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-    frame_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    cap.release()
+        pre, ext = os.path.splitext(filename)
+        if ext == '.txt':
+            txt_path = os.path.join(args.raw_data_dir, pre + ext)
+            video_path = os.path.join(args.raw_data_dir, pre + '.mp4')
+            annot_path = os.path.join(args.raw_data_dir, pre + '.csv')
 
-    df = pd.read_csv(args.annot,
-                     header=None,
-                     names=['Frame',
-                            'Ball',
-                            'bb_left',
-                            'bb_top',
-                            'bb_width',
-                            'bb_height',
-                            'conf',
-                            'wc_x',
-                            'wc_y',
-                            'wc_z'])
+            # open the video file to determine frame size
+            cap = cv2.VideoCapture(video_path)
+            frame_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+            frame_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+            frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+            cap.release()
 
-    df['x'] = (df['bb_left'] + df['bb_width']/2) / frame_width
-    df['y'] = (df['bb_top'] + df['bb_height']/2) / frame_height
+            df = pd.read_csv(txt_path,
+                             header=None,
+                             names=['Frame',
+                                    'Ball',
+                                    'bb_left',
+                                    'bb_top',
+                                    'bb_width',
+                                    'bb_height',
+                                    'conf',
+                                    'wc_x',
+                                    'wc_y',
+                                    'wc_z'])
 
-    # reindex to fill in missing frames
-    full_index = pd.Index(range(0, frame_count), name='Frame')
-    df = df.set_index('Frame').reindex(full_index).reset_index()
-    df.loc[df['Ball'].isna(), ['Ball', 'x', 'y']] = [0, -1, -1]
-    df['Ball'] = df['Ball'].astype('int')
+            df['x'] = (df['bb_left'] + df['bb_width']/2) / frame_width
+            df['y'] = (df['bb_top'] + df['bb_height']/2) / frame_height
 
-    df[['Frame', 'Ball', 'x', 'y']].to_csv(pre + '.csv', index=False)
+            # reindex to fill in missing frames
+            full_index = pd.Index(range(0, frame_count), name='Frame')
+            df = df.set_index('Frame').reindex(full_index).reset_index()
+            df.loc[df['Ball'].isna(), ['Ball', 'x', 'y']] = [0, -1, -1]
+            df['Ball'] = df['Ball'].astype('int')
+
+            df[['Frame', 'Ball', 'x', 'y']].to_csv(annot_path, index=False)
