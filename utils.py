091e523bbcafe520e9c4e4e05c32355f7ba49324
stevearonson <steve_aronson@yahoo.com>
2022-02-14 12:08:05 -0500

    Update findCountours
    Reduced return variable count from 3 to 2

--- utils.py
+++ utils.py
@@ -195,7 +195,8 @@ def confusion(y_pred, y_true, tol):
             (cx_pred, cy_pred) = (int(x+w/2), int(y+h/2))
 
             # find center of ball for ground truth
-            _, contours, _ = cv2.findContours(h_true.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            # _, contours, _ = cv2.findContours(h_true.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            contours, _ = cv2.findContours(h_true.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
             bboxes = [cv2.boundingRect(cnt) for cnt in contours]
 
             areas = np.array([bbox[2] * bbox[3] for bbox in bboxes])
